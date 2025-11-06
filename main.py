5557c7a907c03946374fbcaf41c9e9a4b701efa4
dhruvv-t <dhruv.t@keystride.com>
2025-11-06 16:41:31 +0530

    add directory structure displayer

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 from standalone_scripts.generate_sales_6m import generate_sales_data
 from end_to_end.workflow.file_paths import paths
 from end_to_end.workflow.process_new_sales import process_sales
+from standalone_scripts.generate_directory_tree import print_tree
+
+print("Here is the project's File Structure: ")
+root_dir = os.getcwd()
+print_tree(root_dir)
 
 if not os.path.exists(paths['sales']):
     generate_sales_data()
---
+++ standalone_scripts/generate_directory_tree.py
@@ -0,0 +1,16 @@
+import os
+
+def print_tree(startpath, prefix=""):
+    items = sorted(os.listdir(startpath))
+    items = [i for i in items if i != ".git"]  # Ignore .git folder
+
+    for index, item in enumerate(items):
+        path = os.path.join(startpath, item)
+        connector = "├─" if index < len(items) - 1 else "└─"
+        if os.path.isdir(path):
+            print(f"{prefix}{connector} Dir: {item}")
+            # Continue deeper with updated prefix
+            extension = "│  " if index < len(items) - 1 else "   "
+            print_tree(path, prefix + extension)
+        else:
+            print(f"{prefix}{connector} File: {item}")
