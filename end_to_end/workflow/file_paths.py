c2d983bc598303367c6999e56ee731994307f060
dhruvv-t <dhruv.t@keystride.com>
2025-11-09 19:01:03 +0530

    Differentiate the old data from new

--- end_to_end/workflow/file_paths.py
+++ end_to_end/workflow/file_paths.py
@@ -1,9 +1,9 @@
 paths = {
-    'sales' : 'end_to_end/datasets/original/sales.csv',
+    'sales' : 'end_to_end/datasets/original/sales_old.csv',
     'products' : 'end_to_end/datasets/original/products.csv',
     'customers' : 'end_to_end/datasets/original/customers.csv',
     'employees' : 'end_to_end/datasets/original/employees.csv',
     'categories' : 'end_to_end/datasets/original/categories.csv',
     'cities' : 'end_to_end/datasets/original/cities.csv',
-    'sales_pro' : 'end_to_end/datasets/processed/sales_pro.csv',
+    'sales_pro' : 'end_to_end/datasets/processed/sales_pro_old.csv',
 }
