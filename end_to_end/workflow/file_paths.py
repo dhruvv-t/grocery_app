499d832d8610b50a92f2974e811ba24c60ff2bbd
dhruvv-t <dhruv.t@keystride.com>
2025-11-17 17:42:53 +0530

    calculate_reorder_level -> line 48: error here, add content

--- end_to_end/workflow/file_paths.py
+++ end_to_end/workflow/file_paths.py
@@ -3,5 +3,6 @@ paths = {
     'employees' : 'end_to_end/datasets/original/employees.csv', 'categories' : 'end_to_end/datasets/original/categories.csv', 'cities' : 'end_to_end/datasets/original/cities.csv',
 
     'sales_pro' : 'end_to_end/datasets/processed/sales_pro.csv', 'cat_feature_vector' : 'end_to_end/datasets/processed/products_feature_vector.csv',
-    'demand_cats' : 'end_to_end/datasets/original/demand_categories.csv', 'catzd_products' : 'end_to_end/datasets/processed/categorized_products.csv'
+    'demand_cats' : 'end_to_end/datasets/original/demand_categories.csv', 'catzd_products' : 'end_to_end/datasets/processed/categorized_products.csv',
+    'reorder_levels': 'end_to_end/datasets/processed/reorder_level.csv',
 }
--- end_to_end/workflow/inventory_mgmt/calculate_reorder_level.py
+++ end_to_end/workflow/inventory_mgmt/calculate_reorder_level.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import calendar
+import math
 
 from end_to_end.workflow.file_paths import paths
 
@@ -18,13 +20,48 @@ def calculate_safety_stock():
     sales, products = load_dataset()
 
     one_year_stats = (
-        sales[sales['sales_date'].dt.year == 2023].groupby(['product_id', 'month', 'week_start', 'date'])['quantity'].sum().reset_index(name='daily_sales')
+        (
+            sales[sales['sales_date'].dt.year == 2023]
+        ).groupby(['product_id', 'month', 'week_start', 'date'])['quantity']
+        .sum().reset_index(name='daily_sales')
     ).groupby(['product_id'])['daily_sales'].agg(avg_daily_sales='mean', max_daily_sales='max').rest_index()
 
-    one_year_stats['safety_stock'] = ((one_year_stats['max_daily_sales'] - one_year_stats['avg_daily_sales']) * products['lead_time_days']).round(0)
+    one_year_stats['safety_stock'] = (
+        (one_year_stats['max_daily_sales'] - one_year_stats['avg_daily_sales']) * products['lead_time_days']
+    ).round(0)
 
-    return one_year_stats[['product_id', 'safety_stock']]
+    products = (products.merge(one_year_stats[['products_id', 'safety_stock']], on='product_id', how='left'))[['product_id', 'product_name', 'vitality_days', 'lead_time_days', 'safety_stock']]
+    return products, sales
 
+
+def initialize_reorder_level():
+
+    categorized_products = pd.read_csv(paths['catzd_products'])
+    products, sales = calculate_safety_stock()
+
+    sales_to_analyze = sales[sales['sales_date'].dt.year == 2023].merge(
+        categorized_products, on='product_id', how='left'
+    )
+    products = products.merge(categorized_products, on='product_id', how='left')
+
+    stable_reorder_initialize = (sales_to_analyze[sales_to_analyze['category'] == 'Stable'].groupby('product_id')['quantity'].sum() / 364).reset_index()
+    stable_reorder_initialize['recalculate_on'] = sales_to_analyze
+    trending_reorder_initialize = (sales_to_analyze[
+        (sales_to_analyze['category'] == 'Trending') & (sales_to_analyze['sales_date'].dt.month == sales_to_analyze['sales_date'].max().month)
+    ].groupby('product_id')['quantity'].sum() / calendar.monthrange(sales_to_analyze['sales_date'].max().year, sales_to_analyze['sales_date'].max().month)[1]).reset_index()
+    volatile_reorder_initialize = (sales_to_analyze[
+        (sales_to_analyze['category'] == 'Volatile') & (sales_to_analyze['sales_date'] >= (sales_to_analyze['sales_date'].max() - pd.Timedelta(days=14)))
+    ].groupby('product_id')['quantity'].sum() / 14).reset_index()
+
+    products = products.merge(
+        pd.concat([stable_reorder_initialize, trending_reorder_initialize, volatile_reorder_initialize], ignore_index=True), on='product_id', how='left'
+    ).rename(columns={'quantity': 'avg_daily_demand'}).fillna(0)
+
+    products['avg_daily_demand'] = products['avg_daily_demand'].apply(math.ceil)
+    products['reorder_level'] = (products['avg_daily_demand'] * products['lead_time_days']) + products['safety_stock']
+    
+    return products
 
 def calculate_reorder_level():
-    print('calculate Reorder Level.')
+    recalculate = pd.read_csv(paths['reorder_levels'])
+
