35863cf69292b3b6c335051fb69ba6d1c3ce663c
dhruvv-t <dhruv.t@keystride.com>
2025-11-24 11:58:13 +0530

    uncomment important parts

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ print("Here is the project's File Structure: ")
 root_dir = os.getcwd()
 generate_directory_tree.print_tree(root_dir)
 
-# # Generating the dataset if it doesn't exits
-# if not os.path.exists(file_paths.paths['sales']):
-#     realistic_dataset_generator.generate_sales_data()
+# Generating the dataset if it doesn't exits
+if not os.path.exists(file_paths.paths['sales']):
+    realistic_dataset_generator.generate_sales_data()
 
-# # Pre-processing the sales data
-# process_new_sales.process_sales()
+# Pre-processing the sales data
+process_new_sales.process_sales()
 
-# # Categorize products into categories based on their sales patterns
-# categorize_products.categorize_products_on_sales()
+# Categorize products into categories based on their sales patterns
+categorize_products.categorize_products_on_sales()
 
-# # Calculate Reorder Levels and Quantities for each product
-# calculate_reorder_specifications.initialize_reorder_level()
+# Calculate Reorder Levels and Quantities for each product
+calculate_reorder_specifications.initialize_reorder_level()
